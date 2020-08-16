f0a1bbcaecc40b18227b89ecf81a45dac795538a
Jan Tymiński <tymikk@gmail.com>
2020-08-16 19:53:28 +0200

    first API fetch attempt

--- frontend/src/Model.js
+++ frontend/src/Model.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Modal } from "react-bootstrap";
+import {MyComponent} from "./Fetcher";
 import os from 'os';
 
 export default function Model({ handleClick, status }) {
   // var hostname = os.hostname();
-  var hostname = os.platform();
-  console.log(hostname);
+  // var hostname = os.platform();
+  // console.log(hostname);
+  var appname = window.navigator.appName;
+  var type = os.type();
+  var release = os.release();
+  var hostname = os.hostname();
+  var arch = os.arch();
+  var platform = os.platform();
 
   return (
     <>
@@ -14,7 +21,14 @@ export default function Model({ handleClick, status }) {
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal! + { hostname }</Modal.Body>
+        <Modal.Body>
+        <p>browser/navigator/?: { appname } </p>
+        <p>type: { type }</p>
+        <p>release: { release }</p>
+        <p>hostname: { hostname }</p>
+        <p>arch: { arch }</p>
+        <p>platform: { platform }</p>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClick}>
             Close
@@ -29,6 +43,8 @@ export default function Model({ handleClick, status }) {
 }
 
   export function Model2({ handleClick, status }) {
+    var fetchedHostname = MyComponent();
+    console.log(fetchedHostname);
     return (
       <>
         <Modal show={status} onHide={handleClick}>
